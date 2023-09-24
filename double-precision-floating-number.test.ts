b961ac6d0a43e992e17896ea0ac06df23103df65
Matan Yadaev <matan.yed@gmail.com>
2023-09-25 00:59:11 +0300

    toBinary impl

--- double-precision-floating-number.test.ts
+++ double-precision-floating-number.test.ts
@@ -1,7 +1,63 @@
 import {describe, test, expect} from "bun:test";
-import {toDecimal} from "./double-precision-floating-point.ts";
+import {toBinary, toDecimal} from "./double-precision-floating-point.ts";
 
 describe('doublePrecisionFloatingPoint', () => {
+    describe('toBinary', () => {
+        test.each([
+            [123, '0100000001011110110000000000000000000000000000000000000000000000'],
+            [0, '0000000000000000000000000000000000000000000000000000000000000000'],
+        ])('Positive integer', (decimal, expected) => {
+            // Act
+            const result = toBinary(decimal);
+
+            // Assert
+            expect(result).toBe(expected);
+        });
+
+        test.each([
+            [-123, '1100000001011110110000000000000000000000000000000000000000000000'],
+            [-0, '1000000000000000000000000000000000000000000000000000000000000000'],
+        ])('Negative integer', (decimal, expected) => {
+            // Act
+            const result = toBinary(decimal);
+
+            // Assert
+            expect(result).toBe(expected);
+        })
+
+        test.each([
+            [123.456, '0100000001011110110111010010111100011010100111111011111001110111'],
+        ])('Positive decimal', (decimal, expected) => {
+            // Act
+            const result = toBinary(decimal);
+
+            // Assert
+            expect(result).toBe(expected);
+        })
+
+        test.each([
+            [-123.456, '1100000001011110110111010010111100011010100111111011111001110111'],
+        ])('Negative decimal', (decimal, expected) => {
+            // Act
+            const result = toBinary(decimal);
+
+            // Assert
+            expect(result).toBe(expected);
+        })
+
+        test.each([
+            [Number.POSITIVE_INFINITY, '0111111111110000000000000000000000000000000000000000000000000000'],
+            [Number.NEGATIVE_INFINITY, '1111111111110000000000000000000000000000000000000000000000000000'],
+            [Number.NaN, '0111111111111000000000000000000000000000000000000000000000000000'],
+        ])('Special values', (decimal, expected) => {
+            // Act
+            const result = toBinary(decimal);
+
+            // Assert
+            expect(result).toBe(expected);
+        })
+    });
+
     describe('toDecimal', () => {
         test.each([
             ['0100000001011110110000000000000000000000000000000000000000000000', 123],
--- double-precision-floating-point.ts
+++ double-precision-floating-point.ts
@@ -1,3 +1,47 @@
+const EXPONENT_BIAS = 1023;
+
+export const toBinary = (number: number): string => {
+    if (number === Number.POSITIVE_INFINITY) {
+        return '0111111111110000000000000000000000000000000000000000000000000000';
+    }
+
+    if (number === Number.NEGATIVE_INFINITY) {
+        return '1111111111110000000000000000000000000000000000000000000000000000';
+    }
+
+    if (Number.isNaN(number)) {
+        return '0111111111111000000000000000000000000000000000000000000000000000';
+    }
+
+    if (number === 0) {
+        const sign = 1 / number === -Infinity ? '1' : '0';
+
+        return sign + '000000000000000000000000000000000000000000000000000000000000000';
+    }
+
+    const sign = number < 0 ? '1' : '0';
+    number = Math.abs(number);
+
+    let exponent = Math.floor(Math.log2(number));
+    let fraction = number / Math.pow(2, exponent) - 1;
+
+    let exponentBinary = (exponent + EXPONENT_BIAS).toString(2).padStart(11, '0');
+
+    let fractionBinary = '';
+    for (let i = 0; i < 52 && fraction > 0; i++) {
+        fraction *= 2;
+        if (fraction >= 1) {
+            fractionBinary += '1';
+            fraction -= 1;
+        } else {
+            fractionBinary += '0';
+        }
+    }
+    fractionBinary = fractionBinary.padEnd(52, '0');
+
+    return sign + exponentBinary + fractionBinary;
+};
+
 export const toDecimal = (binary: string): number => {
     if (binary.length !== 64) {
         throw new Error('Invalid binary string');
@@ -11,7 +55,7 @@ export const toDecimal = (binary: string): number => {
     const exponentBinary = binary.slice(1, 12);
     const fractionBinary = binary.slice(12);
 
-    const exponent = parseInt(exponentBinary, 2) - 1023;
+    const exponent = parseInt(exponentBinary, 2) - EXPONENT_BIAS;
 
     let fraction = exponent === -1023 ? 0 : 1;
 
@@ -22,9 +66,11 @@ export const toDecimal = (binary: string): number => {
     if (exponent === 1024 && fraction > 1) {
         return Number.NaN;
     }
+
     if (exponent === 1024 && fraction === 0) {
         return sign === 1 ? Number.POSITIVE_INFINITY : Number.NEGATIVE_INFINITY;
     }
+
     if (exponent === -1023 && fraction === 0) {
         return sign === 1 ? 0 : -0;
     }
